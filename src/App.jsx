4af6dbd8478e76db0949ee41440d4b28096d75cd
Avinash <palakurthiavinash07@gmail.com>
2023-09-16 10:06:05 +0530

    instaClone setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./components/Login";
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,15 @@
+import Carousel from "./Carousel";
+import LoginForm from "./LoginForm";
+
+const index = () => {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <Carousel />
+
+      <LoginForm />
+    </>
+  );
+};
+
+export default index;
